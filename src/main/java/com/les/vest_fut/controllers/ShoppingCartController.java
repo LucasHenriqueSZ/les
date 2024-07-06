ba7ede02c8ee4bf457017ccd9838577ab481b3e3
lucas <lucash100souza@gmail.com>
2024-07-06 19:09:39 -0300

    feat: Adicionado a interface de carrinho de compra

---
+++ src/main/java/com/les/vest_fut/controllers/ShoppingCartController.java
@@ -0,0 +1,15 @@
+package com.les.vest_fut.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/carrinho-compras")
+public class ShoppingCartController {
+
+    @RequestMapping()
+    public ModelAndView shoppingCart() {
+        return new ModelAndView("public/shopping-cart");
+    }
+}
