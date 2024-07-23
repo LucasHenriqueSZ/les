4cb0b61a29f83e0e7be32a819f671d913ca30344
lucas <lucash100souza@gmail.com>
2024-07-22 23:58:59 -0300

    feat: Adicionado configuração para separação de arquivo css do admin e adição da minificação dos arquivos de css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 const path = require('path');
 
 const paths = {
     styles: {
         src: 'src/main/resources/static/scss/custom.scss',
+        dest: 'src/main/resources/static/css/'
+    },
+    styles_adm: {
+        src: 'src/main/resources/static/scss/admin/custom-adm.scss',
         dest: 'src/main/resources/static/css/'
     },
     scripts: {
@@ -19,11 +25,15 @@ const paths = {
 };
 
 gulp.task('sass', function () {
-    return gulp.src(paths.styles.src)
+    return gulp.src([paths.styles.src, paths.styles_adm.src])
         .pipe(sass({
             includePaths: [path.join(__dirname, 'node_modules')]
         }).on('error', sass.logError))
-        .pipe(gulp.dest(paths.styles.dest));
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(function (file) {
+            return file.base.includes('admin') ? paths.styles_adm.dest : paths.styles.dest;
+        }));
 });
 
 gulp.task('scripts', function () {
