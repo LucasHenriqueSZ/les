37c0ebe1617774e40ed3c5836d5acf9f4f41e6ca
lucas <lucash100souza@gmail.com>
2024-09-17 20:46:21 -0300

    feat: migrations para população de produtos e usuario comum adicionado

---
+++ src/main/resources/db/migration/V15__Insert_sample_tshirt_products.sql
@@ -0,0 +1,7 @@
+INSERT INTO products (pdt_description, pdt_name, pdt_price, pdt_size)
+VALUES
+    ('Camiseta de algodão básica', 'Camiseta Básica', 29.99, 'P'),
+    ('Camiseta de algodão com estampa frontal', 'Camiseta Estampada', 34.99, 'M'),
+    ('Camiseta de manga longa', 'Camiseta Manga Longa', 39.99, 'G'),
+    ('Camiseta esportiva de poliéster', 'Camiseta Esportiva', 44.99, 'GG'),
+    ('Camiseta com gola V', 'Camiseta Gola V', 27.99, 'P');
