b613adb7fb9934c388c52b483d0157679492d0e0
lucas <lucash100souza@gmail.com>
2024-09-26 23:14:45 -0300

    feat: Adicionado entidade para cupon

---
+++ src/main/java/com/les/vest_fut/model/payment/Cupon.java
@@ -0,0 +1,54 @@
+package com.les.vest_fut.model.payment;
+
+import com.les.vest_fut.model.users.UserEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cupons")
+public class Cupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cup_id")
+    private Long id;
+
+    @NotNull
+    @Size(min = 5, max = 20)
+    @Column(name = "cup_code", length = 20, nullable = false)
+    private String code;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    @Column(name = "cup_type", nullable = false)
+    private CuponType type;
+
+    @DecimalMin("0.0")
+    @DecimalMax("100.0")
+    @Column(name = "cup_discount_percentage", precision = 5, scale = 2)
+    private BigDecimal discountPercentage;
+
+    @DecimalMin("0.0")
+    @Column(name = "cup_amount", precision = 10, scale = 2)
+    private BigDecimal amount;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "usr_id", nullable = false)
+    private UserEntity user;
+
+    @Column(name = "cup_used", nullable = false)
+    private boolean used;
+}
