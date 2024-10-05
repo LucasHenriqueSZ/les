7af6df37423739568073e55e5f8d68544eaf2c86
lucas <lucash100souza@gmail.com>
2024-10-05 16:44:16 -0300

    feat: Adicionado a helper a paagina de solicitação de troca

--- src/main/java/com/les/vest_fut/controllers/helpers/OrderControllerHelper.java
+++ src/main/java/com/les/vest_fut/controllers/helpers/OrderControllerHelper.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-public class CheckoutControllerHelper extends CommonControllerHelper {
+public class OrderControllerHelper extends CommonControllerHelper {
 
     public static ModelAndView redirectMyOrders() {
         return new ModelAndView("redirect:/pedidos");
@@ -16,9 +16,17 @@ public class CheckoutControllerHelper extends CommonControllerHelper {
         return new ModelAndView("redirect:/checkout");
     }
 
+    public static ModelAndView redirectOrdersUser(){return new ModelAndView("redirect:/pedidos");}
+
     public static ModelAndView prepareOrdersUser(List<Order> orders) {
         ModelAndView mv = new ModelAndView("public/pages/user/orders");
         mv.addObject("orders", orders);
+        return mv;
+    }
+
+    public static ModelAndView prepareExchangeRequestPage(Order order) {
+        ModelAndView mv = new ModelAndView("public/pages/user/exchange-request-page");
+        mv.addObject("order", order);
         return mv;
     }
 }
